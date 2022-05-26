44956244102f85e960d945fcea98f0b9cbb88e27
John Netzel <johnrnetzel@gmail.com>
2022-05-26 07:46:14 -0800

    Add ability for app to be installed as PWA from clicking install button

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { GenerateSW } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -22,7 +19,23 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
-      new GenerateSW()
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        name: 'Progressive Text Editor',
+        short_name: 'PTE',
+        description: 'Keep track of important tasks and snippets!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
